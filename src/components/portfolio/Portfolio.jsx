9e84291744a6567cc910463c41ce63cf76843433
leomensah <leomensah.main@gmail.com>
2023-03-09 14:20:52 +0000

    Testimonials section added with the image button fixed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,65 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: "https://dribble.com/Alien_pixels"
+
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: "https://dribble.com/Alien_pixels"
+
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: "https://dribble.com/Alien_pixels"
+
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: "https://dribble.com/Alien_pixels"
+
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: "https://dribble.com/Alien_pixels"
+
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Crypto Currency Dashboard',
+    github: 'https://github.com',
+    demo: "https://dribble.com/Alien_pixels"
+
+  }
+]
+
 
 const Portfolio = () => {
   return (
@@ -8,14 +68,23 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <h3>This is a portfolio item title</h3>
-            <a href="https://github.com" className='btn'>Github</a>
-          </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+            <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn' target='_blank'>Github</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
+          </article>
+            )
+          })
+        }
       </div>
-
     </section>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \
+    Possimus delectus accusamus natus aliquam explicabo atque aliquid \
+    odio facere, ratione fuga? Placeat eius doloribus corrupti \
+    temporibus officia quam inventore mollitia dolore.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \
+    Possimus delectus accusamus natus aliquam explicabo atque aliquid \
+    odio facere, ratione fuga? Placeat eius doloribus corrupti \
+    temporibus officia quam inventore mollitia dolore.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \
+    Possimus delectus accusamus natus aliquam explicabo atque aliquid \
+    odio facere, ratione fuga? Placeat eius doloribus corrupti \
+    temporibus officia quam inventore mollitia dolore.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. \
+    Possimus delectus accusamus natus aliquam explicabo atque aliquid \
+    odio facere, ratione fuga? Placeat eius doloribus corrupti \
+    temporibus officia quam inventore mollitia dolore.'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className="container testimonials__container"
+      modules={[ Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar}/>
+              </div>
+              <h5 className='client__name'>{name}</h5>
+              <small className='client__review'>
+                  {review}
+              </small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
